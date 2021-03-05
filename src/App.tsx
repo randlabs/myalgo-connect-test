a204c2b01c1c88775bbb2ea5ccef4e24dd13eda8
JCibeira <jacibcas@gmail.com>
2021-03-05 15:55:21 -0400

    Added connect and payment

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
 import React, { Component, ReactNode } from 'react';
-import { Button } from 'reactstrap';
-import PrismCode from './components/code/Code';
-import './App.css';
+import MyAlgo, { Accounts } from '@randlabs/myalgo-connect';
 
-import logo from './assets/images/MyAlgo.svg'
+import './App.scss';
 
-class App extends Component<{}> {
+import Connect from './components/connect/Connect';
+import Payment from './components/payment/Payment';
+//import logo from './assets/images/MyAlgo.svg';
+
+interface IAppState {
+    accounts: Accounts[];
+}
+
+class App extends Component<{}, IAppState> {
+    private connection: MyAlgo;
+
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            accounts: []
+        }
+
+        this.connection = new MyAlgo();
+
+        this.onCompleteConnect = this.onCompleteConnect.bind(this);
+    }
+
+    onCompleteConnect(accounts: Accounts[]): void {
+        this.setState({
+            accounts
+        })
+    }
+
     render(): ReactNode {
-        const code = `
-        const foo = 'foo';
-const bar = 'bar';
-console.log(foo + bar);
-        `;
+        const { accounts } = this.state;
+
         return (
             <div className="app">
-                <img src={logo} className="myalgo-logo" alt="MyAlgo logo" />
-                <PrismCode
-                    code={code}
-                    language="js"
+                {/* <Row className="mb-5">
+                        <Col>
+                            <img src={logo} className="myalgo-logo" alt="MyAlgo logo" />
+                        </Col>
+                    </Row> */}
+                <Connect
+                    connection={this.connection}
+                    onComplete={this.onCompleteConnect}
                 />
-                <Button color="danger">
-                    Danger!
-                </Button>
+                <Payment
+                    connection={this.connection}
+                    accounts={accounts}
+                />
             </div>
         );
     }
