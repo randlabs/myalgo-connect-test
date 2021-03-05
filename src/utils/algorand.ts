a54a5b7f0cfb14f9b17a85e82f5df466740b3ee9
JCibeira <jacibcas@gmail.com>
2021-03-05 15:53:58 -0400

    Added utils

---
+++ src/utils/algorand.ts
@@ -0,0 +1,18 @@
+import algosdk from 'algosdk';
+
+export function fromDecimal(value: string|number, decimal: number = 0): number {
+	if (typeof value === "string") {
+		value = value.trim();
+		value = parseFloat(value) || 0;
+	}
+	const precisionNumber = decimal > 6 ? Math.pow(10, decimal) : 10000;
+	return ( ( (value * precisionNumber) * (Math.pow(10, decimal) ) ) / precisionNumber );
+}
+
+export function validateAddress(address: string): boolean {
+	if (typeof address !== "string") {
+		return false;
+	}
+
+	return algosdk.isValidAddress(address);
+}
